c986bac01e315e7a23e6eafa36935da06cb14fc0
ImGabreuw <imgabreuw.dev@gmail.com>
2021-11-01 20:19:58 -0300

    Initial commit

---
+++ src/test/java/com/amigoscode/beans/Car.java
@@ -0,0 +1,15 @@
+package com.amigoscode.beans;
+
+import lombok.Data;
+
+@Data
+public class Car {
+
+    private final Integer id;
+    private final String make;
+    private final String model;
+    private final String color;
+    private final Integer year;
+    private final Double price;
+
+}
---
+++ src/test/java/com/amigoscode/beans/PersonDTO.java
@@ -0,0 +1,19 @@
+package com.amigoscode.beans;
+
+import lombok.Data;
+
+@Data
+public class PersonDTO {
+
+    private final Integer id;
+    private final String name;
+    private final Integer age;
+
+    public static PersonDTO map(Person person) {
+        return new PersonDTO(
+                person.getId(),
+                person.getFirstName(),
+                person.getAge());
+    }
+
+}
